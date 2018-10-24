be8cdd2a0a16bf9131f44ee7c65b94834a7a3416
raul <rasir.informatica@gmail.com>
2018-10-24 13:43:58 +0100

    a1

--- ut2/a1/main.py
+++ ut2/a1/main.py
@@ -2,16 +2,17 @@ import sys
 
 importe = int(sys.argv[1])
 
-a = 50
-b = 20
-c = 10
-d = 5
-e = 2
-f = 1
+billetes50= importe // 50
+resto50= importe % 50
+billetes20 = resto50 // 20
+resto20= importe % 20
+billetes10 = resto20 // 10
+resto10= importe % 10
+billetes5= resto10 // 5
+resto5= importe % 5
 
-if a = 50:
-    print ('1 billete de a')
-elif a < 50:
-        print ('% billete de a')
-else:
-    print('billetes de ')        
+if importe !=0:
+    if resto == 1:
+        print(billetes50)
+    if importe > 1:
+        print(billetes20)
