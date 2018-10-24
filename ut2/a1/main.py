3faf4d6599aac820336cd1bf5117930da1495da6
raul <rasir.informatica@gmail.com>
2018-10-24 13:25:53 +0100

    a1

---
+++ ut2/a1/main.py
@@ -0,0 +1,17 @@
+import sys
+
+importe = int(sys.argv[1])
+
+a = 50
+b = 20
+c = 10
+d = 5
+e = 2
+f = 1
+
+if a = 50:
+    print ('1 billete de a')
+elif a < 50:
+        print ('% billete de a')
+else:
+    print('billetes de ')        
