31816e7cb18a6881143273d05ff022656b3ef1aa
raul <rasir.informatica@gmail.com>
2018-10-31 13:04:37 +0000

    a2 programa4

---
+++ ut2/a2/programa4.py
@@ -0,0 +1,9 @@
+import sys
+import math
+from math import pi
+
+print('Menú')
+print('(1)Calcular el díametro de la circunferencia')
+print('(2)Calcular el perímetro de la circunferencia')
+print('(3)Calcular el área del circulo')
+print('(4)Salir')
