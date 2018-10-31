19cd8187cd609739bfd1392fb29bf1b765019256
raul <rasir.informatica@gmail.com>
2018-10-31 12:45:00 +0000

    a2 progrma2

--- ut2/a2/programa2.py
+++ ut2/a2/programa2.py
@@ -8,7 +8,7 @@ d = sqrt(((x1-x2)**2)+((y1-y2)**2))
 d1 = sqrt(((x1-x3)**2)+((y1-y3)**2))
 
 if d < d1:
-    print(d, 'Está más cercano  a' (x1-x2), 'es' (y1-y2), 'y está a una distancia de', d)
+    print(d, 'Está más cercano  a', (x1-x2), 'es', (y1-y2), 'y está a una distancia de', d)
 
 if d1 < d:
-    print(d1, 'El punto más cercano a' (x1-x3), 'es' (y1-y3), 'y está a una distancia de', d1)
+    print(d1, 'El punto más cercano a', (x1-x3), 'es', (y1-y3), 'y está a una distancia de', d1)
