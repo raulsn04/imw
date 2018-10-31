72176d544337ea96655f4ac81301db0d25f5234b
raul <rasir.informatica@gmail.com>
2018-10-31 12:37:14 +0000

    a2 programa2

---
+++ ut2/a2/programa2.py
@@ -0,0 +1,15 @@
+import sys
+import math
+from math import sqrt
+
+x1,x2,x3,y1,y2,y3 = float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6])
+
+d = sqrt(((x1-x2)**2)+((y1-y2)**2))
+d1 = sqrt(((x1-x3)**2)+((y1-y3)**2))
+
+if d < d2:
+    print(d, 'Está más cercano  a' (x1-x2), 'es' (y1-y2), 'y está a una distancia de', d)
+
+
+else d2 > d
+    print(d2, 'El punto más cercano a' (x1-x3), 'es' (y1-y3), 'y está a una distancia de', d2)
