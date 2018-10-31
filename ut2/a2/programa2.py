86169fb76ae0bde032788bee091cd7644a883efe
raul <rasir.informatica@gmail.com>
2018-10-31 12:39:10 +0000

    a2 programa2

--- ut2/a2/programa2.py
+++ ut2/a2/programa2.py
@@ -7,9 +7,9 @@ x1,x2,x3,y1,y2,y3 = float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]), 
 d = sqrt(((x1-x2)**2)+((y1-y2)**2))
 d1 = sqrt(((x1-x3)**2)+((y1-y3)**2))
 
-if d < d2:
+if d < d1:
     print(d, 'Está más cercano  a' (x1-x2), 'es' (y1-y2), 'y está a una distancia de', d)
 
 
-else d2 < d
-    print(d2, 'El punto más cercano a' (x1-x3), 'es' (y1-y3), 'y está a una distancia de', d2)
+else d1 < d
+    print(d2, 'El punto más cercano a' (x1-x3), 'es' (y1-y3), 'y está a una distancia de', d1)
