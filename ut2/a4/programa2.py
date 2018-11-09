16f1ec576f3bed5183219d6b6f1d57b604536c37
raul <rasir.informatica@gmail.com>
2018-11-09 10:14:35 +0000

    a4

--- ut2/a4/programa2.py
+++ ut2/a4/programa2.py
@@ -6,7 +6,7 @@ DNA_SIZE = 100
 sequence = ''.join([random.choice(NUCLEOBASES) for i in range(DNA_SIZE)])
 A = 0
 
-for v in range(100):
-    if sequence == 'A':
+for v in sequence:
+    if v == 'A':
         A += 1
 print('Adenine:', A)
