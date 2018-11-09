c91cf160c7d9821ff23a9cfd0d3fda0ae05abec7
raul <rasir.informatica@gmail.com>
2018-11-09 10:09:11 +0000

    a4

--- ut2/a4/programa2.py
+++ ut2/a4/programa2.py
@@ -4,9 +4,9 @@ NUCLEOBASES = "ATGC"
 DNA_SIZE = 100
 
 sequence = ''.join([random.choice(NUCLEOBASES) for i in range(DNA_SIZE)])
-
+A = 0
 
 for v in range(100):
-    if sequence == NUCLEOBASES:
-        NUCLEOBASES += 1
-print('Adenine')
+    if sequence == 'A':
+        A += 1
+print('Adenine:', A)
