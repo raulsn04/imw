51435d0f50dcc88d0ae31c3fc232528cfc198961
raulsn04 <rasir.informatica@gmail.com>
2018-11-12 20:21:06 +0000

    a4 rectificado

--- ut2/a4/programa3.py
+++ ut2/a4/programa3.py
@@ -2,24 +2,14 @@ import sys
 import math
 
 k, cadena = int(sys.argv[1]), sys.argv[2]
-
-
+longitud = 0
 if k < 0:
     print('Error, el número no es positivo')
     sys.exit()
 else:
-    cadena = sys.argv[2]
     palabras = cadena.split(' ')
-    longentrada = 0
 
-    for i in range(len(palabras)):
-        longitud = len(palabras[i])
-        if longitud == k:
-            longentrada += 1
-
-    if longentrada == 0:
-        print(f'No cumple los requisitos {k}')
-    elif longentrada == 1:
-        print(f'La palabra es de tamaño {k}')
-    else:
-        print(f'Hay {longentrada} palabras de tamaño {k}')
+    for i in palabras:
+        if len(i) == k:
+            longitud += 1
+print('Hay esta cantidad', longitud, ' de palabras de', k, 'letras')
