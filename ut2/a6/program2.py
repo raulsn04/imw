3af609f1eaad91b8dfaf701ba87afab4cda65600
raul <rasir.informatica@gmail.com>
2018-11-23 09:57:57 +0000

    a6

---
+++ ut2/a6/program2.py
@@ -0,0 +1,37 @@
+import sys
+
+phone_book = {}
+
+print('Menú')
+print('(1)Mostrar lista de contactos')
+print('(2)Añadir contacto (nombre y telefono)')
+print('(3)Borrar contacto (nombre)')
+print('(4)Salir')
+
+menu=input('Ha elegido la opción: ')
+
+if menu == '1':
+
+    def show_contacts(phone_book):
+        
+        return phone_book
+
+
+
+elif menu == '2':
+
+    def add_contacts(phone_book, name, phone):
+        return
+
+elif menu == '3':
+
+    def remove_contacts(phone_book, name):
+
+        return
+
+elif menu == '4':
+    print('Salir')
+
+
+else:
+    print('Agenda actualizada')
